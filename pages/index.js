770bbcdff20b6fb78acbc9cb2c49cb3bd89a3852
badrulhanif <hanifbaharuddin@gmail.com>
2022-06-01 00:47:11 +0800

    change header title

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ function HomePage() {
   return (
     <>
       <Head>
-        <tittle>Badrul Hanif</tittle>
+        <title>Badrul Hanif</title>
         <meta
           name="description"
           content="Professional profile of Badrul Hanif. You can refer here to see my recent works."
