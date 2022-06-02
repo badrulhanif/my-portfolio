25ea4ac5d712cc9c0238780841b70a72cd60ac1e
badrulhanif <hanifbaharuddin@gmail.com>
2022-06-02 10:07:30 +0800

    add metadata

--- pages/index.js
+++ pages/index.js
@@ -10,6 +10,34 @@ function HomePage() {
           name="description"
           content="Professional profile of Badrul Hanif. You can refer here to see my recent works."
         />
+        {/* Facebook Meta Tags */}
+        <meta
+          property="og:url"
+          content="https://my-portfolio-ashy-xi.vercel.app/"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Badrul Hanif" />
+        <meta
+          property="og:description"
+          content="Professional profile of Badrul Hanif. You can refer here to see my recent works."
+        />
+        <meta property="og:image" content="" />
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:domain"
+          content="my-portfolio-ashy-xi.vercel.app"
+        />
+        <meta
+          property="twitter:url"
+          content="https://my-portfolio-ashy-xi.vercel.app/"
+        />
+        <meta name="twitter:title" content="Badrul Hanif" />
+        <meta
+          name="twitter:description"
+          content="Professional profile of Badrul Hanif. You can refer here to see my recent works."
+        />
+        <meta name="twitter:image" content="" />
       </Head>
       <Portfolio />
     </>
