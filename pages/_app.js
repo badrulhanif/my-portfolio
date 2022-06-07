2b3935af9cde5f7aac0f6c36dac0caed58b2e4be
badrulhanif <hanifbaharuddin@gmail.com>
2022-06-07 22:48:33 +0800

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,23 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
-
-import * as ga from '../lib/ga'
+import "../styles/globals.css";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      ga.pageview(url)
-    }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [router.events])
-
-  return <Component {...pageProps} />
+  return (
+    <>
+      {" "}
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy="lazyOnload" id="">
+        {`window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');`}
+      </Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
