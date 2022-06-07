10af2c1107d8b77aeb16589f1822722ffac2f3b8
badrulhanif <hanifbaharuddin@gmail.com>
2022-06-07 22:11:09 +0800

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,6 @@
 import "../styles/globals.css";
 import Script from "next/script";
 
-// <!-- Global site tag (gtag.js) - Google Analytics -->
-// <script async src="https://www.googletagmanager.com/gtag/js?id=G-H4BR98L7SL"></script>
-// <script>
-//   window.dataLayer = window.dataLayer || [];
-//   function gtag(){dataLayer.push(arguments);}
-//   gtag('js', new Date());
-
-//   gtag('config', 'G-H4BR98L7SL');
-// </script>
-
 function MyApp({ Component, pageProps }) {
   return (
     <>
